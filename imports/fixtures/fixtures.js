f449e64fe1ab7eccfb836c18afb06a66129707b3
Richard Norris <richard.norris@gmail.com>
2019-07-31 17:44:30 +0200

    Integrate selectedNoteId into list items and url

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,15 @@
+export const notes = [
+    {
+        _id: 'noteId1',
+        title: 'Test title',
+        body: '',
+        updatedAt: 1564496648171,
+        userId: 'userId1'
+    }, {
+        _id: 'noteId2',
+        title: '',
+        body: 'Something is here',
+        updatedAt: 1564496648171,
+        userId: 'userId2'
+    }
+];
