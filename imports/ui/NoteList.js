35b3bcd1c1cced248cfd9d92250f5336bc009770
Richard Norris <richard.norris@gmail.com>
2019-07-31 15:40:40 +0200

    Render empty item if no notes

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -3,22 +3,22 @@ import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import NoteListHeader from './NoteListHeader';
 import NoteListItem from './NoteListItem';
-
+import NoteListEmptyItem from './NoteListEmptyItem';
 
 import { Notes } from '../api/notes';
 
 export const NoteList = (props) => {
-   
     return (
         <div>
             <NoteListHeader/>
+            { props.notes.length === 0 ? <NoteListEmptyItem/> : undefined }
             {props.notes.map((note) => {
-                return <NoteListItem key={note._id} note={note}/>
+                return <NoteListItem key={note._id} note={note}/>;
             })}
             NoteList { props.notes.length }
         </div>
     );
-};
+   };
 
 NoteList.propTypes = {
     notes: React.PropTypes.array.isRequired
